3b12f22f380cc4a5a4f0c20f2f1e7ff5b5e54f13
Shawn Stolsig <shawnstolsig@gmail.com>
2020-08-12 00:15:13 -0700

    add text channel filter for commands

--- events/message.js
+++ events/message.js
@@ -59,12 +59,15 @@ module.exports = async (client, message) => {
   // To simplify message arguments, the author's level is now put on level (not member so it is supported in DMs)
   // The "level" command module argument will be deprecated in the future.
   message.author.permLevel = level;
-  
+
   message.flags = [];
   while (args[0] && args[0][0] === "-") {
     message.flags.push(args.shift().slice(1));
   }
-  // If the command exists, **AND** the user has permission, run it.
-  client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
-  cmd.run(client, message, args, level);
+  // If the command exists, **AND** the user has permission, ***AND*** command comes through correct channel, run it.
+  if (message.channel.id === client.botChannelId) {
+    client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
+    cmd.run(client, message, args, level);
+  }
+
 };
